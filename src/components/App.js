2d082ab1a99bff9afe0904d300df4191aa97a5e5
john <johnmaluki58@gmail.com>
2023-07-21 02:05:01 +0300

    Complete assignment on [COMPULSORY] Putting it All Together: State and Events

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,64 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskItems, setTaskItems] = useState(TASKS);
+  const [currectSelectedButton, setCurrectSelectedButton] = useState("All");
+  const [buttonState, setButtonState] = useState({
+    All: true,
+    Code: false,
+    Food: false,
+    Money: false,
+    Misc: false,
+  });
+
+  const changeSelectedButton = (category) => {
+    setCurrectSelectedButton(category);
+    setButtonState({
+      ...buttonState,
+      [currectSelectedButton]: false,
+      [category]: true,
+    });
+  };
+
+  const deleteTask = (taskIndex) => {
+    const newTasks = taskItems.filter((_, index) => {
+      if (index !== taskIndex) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    setTaskItems(newTasks);
+  };
+
+  const addTask = (task) => {
+    const newTasks = [...taskItems, task];
+    setTaskItems(newTasks);
+    console.log(newTasks);
+  };
+
+  const filtedTasks = taskItems.filter((task) => {
+    if (currectSelectedButton === "All") {
+      return true;
+    } else {
+      return task.category === currectSelectedButton;
+    }
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        buttonState={buttonState}
+        onSelectedButtonChange={changeSelectedButton}
+      />
+      <NewTaskForm
+        categories={CATEGORIES.slice(1)}
+        onTaskFormSubmit={addTask}
+      />
+      <TaskList tasks={filtedTasks} onTaskDelete={deleteTask} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,30 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, buttonState, onSelectedButtonChange }) {
+  const buttons = categories.map((category) => {
+    const active = buttonState[category];
+    if (active) {
+      return (
+        <button
+          className="selected"
+          key={category}
+          onClick={() => onSelectedButtonChange(category)}
+        >
+          {category}
+        </button>
+      );
+    } else {
+      return (
+        <button key={category} onClick={() => onSelectedButtonChange(category)}>
+          {category}
+        </button>
+      );
+    }
+  });
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {buttons}
     </div>
   );
 }
